84afabe28964f32d402c40e0e02a9e3bba7137a8
S0f1eM <s.marchand56@gmail.com>
2020-06-20 10:23:50 +0200

    remote add

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization:
-      'Client-ID c45666845627604112ca45c62687928decd09c4082fba6a46cb8e9fee61879ab'
+      ''
   }
 });
